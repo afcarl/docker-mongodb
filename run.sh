bde17a3ca610f4132b1a957e38e34282e827212a
Frank Lemanschik <frank@github.dspeed.eu>
2014-10-07 20:25:45 +0200

    Switched to use mongodb User

--- run.sh
+++ run.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+set -e
+
+chown -R mongodb:mongodb /data/db
+
 if [ ! -f /.mongodb_password_set ]; then
 	/set_mongodb_password.sh
 fi
 if [ ! -f /data/db/mongod.lock ]; then
-exec /usr/bin/mongod --nojournal --auth --rest
+exec sudo -u mongodb -H /usr/bin/mongod --config /etc/mongod.conf --httpinterface --rest --nojournal --auth --smallfiles
 else
 rm /data/db/mongod.lock
-mongod --dbpath /data/db --repair && exec /usr/bin/mongod --nojournal --auth --rest --dbpath /data/db
+mongod --dbpath /data/db --repair && exec sudo -u mongodb -H /usr/bin/mongod --config /etc/mongod.conf --httpinterface --rest --nojournal --auth --smallfiles --dbpath /data/db
 fi
 
