ec76b8b3fd76563da2c2dfd44a41504e0c47204b
Frank Lemanschik <frank@github.dspeed.eu>
2014-10-08 02:17:21 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-chown -R mongodb:mongodb /data/db
+chown -R mongodb:mongodb /var/lib/mongodb
 
 if [ ! -f /.mongodb_password_set ]; then
 	/set_mongodb_password.sh
 fi
-if [ ! -f /data/db/mongod.lock ]; then
+if [ ! -f /var/lib/mongodb/mongod.lock ]; then
 exec sudo -u mongodb -H /usr/bin/mongod --config /etc/mongod.conf --httpinterface --rest --nojournal --auth --smallfiles
 else
-rm /data/db/mongod.lock
-mongod --dbpath /data/db --repair && exec sudo -u mongodb -H /usr/bin/mongod --config /etc/mongod.conf --httpinterface --rest --nojournal --auth --smallfiles --dbpath /data/db
+rm /var/lib/mongodb/mongod.lock
+mongod --dbpath /data/db --repair && exec sudo -u mongodb -H /usr/bin/mongod --config /etc/mongod.conf --httpinterface --rest --nojournal --auth --smallfiles --dbpath /var/lib/mongodb
 fi
 
