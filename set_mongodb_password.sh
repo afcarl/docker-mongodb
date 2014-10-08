770577c6023085a9bd2462c164270c31d2cfcf0b
Frank Lemanschik <frank@github.dspeed.eu>
2014-10-08 14:41:31 +0200

    Update set_mongodb_password.sh

--- set_mongodb_password.sh
+++ set_mongodb_password.sh
@@ -15,6 +15,7 @@ RET=1
 while [[ RET -ne 0 ]]; do
     echo "=> Waiting for confirmation of MongoDB service startup"
     sleep 5
+    ps aux
     mongo admin --eval "help" >/dev/null 2>&1
     RET=$?
 done
